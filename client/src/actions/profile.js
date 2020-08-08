604efe9a8424fab4defa263b58ab0c2c44136e5b
Brandon Endres <17bendres@gmail.com>
2020-08-08 17:51:00 -0400

    Finished profile actions and adds display profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED } from './types';
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE, CLEAR_PROFILE, ACCOUNT_DELETED, GET_PROFILES, GET_REPOS } from './types';
 
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -10,6 +10,58 @@ export const getCurrentProfile = () => async dispatch => {
 
         dispatch({
             type: GET_PROFILE,
+            payload: response.data
+        });
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        });
+    }
+};
+
+// Get all users profiles
+export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
+    try {
+        const response = await axios.get('/api/profile');
+
+        dispatch({
+            type: GET_PROFILES,
+            payload: response.data
+        });
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        });
+    }
+};
+
+// Get all users profiles
+export const getProfile = id => async dispatch => {
+    try {
+        const response = await axios.get(`/api/profile/user/${id}`);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: response.data
+        });
+    } catch (error) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: error.response.statusText, status: error.response.status }
+        });
+    }
+};
+
+// Get Github repos
+export const getGithubRepos = username => async dispatch => {
+    try {
+        const response = await axios.get(`/api/profile/github/${username}`);
+
+        dispatch({
+            type: GET_REPOS,
             payload: response.data
         });
     } catch (error) {
@@ -163,7 +215,7 @@ export const deleteEducation = id => async dispatch => {
 export const deleteAccount = () => async dispatch => {
     if (window.confirm('Are you sure? This can not be undone.')) {
         try {
-            const response = await axios.delete('/api/profile');
+            await axios.delete('/api/profile');
     
             dispatch({ type: CLEAR_PROFILE });
             dispatch({ type: ACCOUNT_DELETED });
--- client/src/components/layout/Spinner.js
+++ client/src/components/layout/Spinner.js
@@ -1,4 +1,4 @@
-import React, { fragment, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import spinner from './spinner.gif';
 
 export default () => (
